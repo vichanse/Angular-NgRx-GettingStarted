bb8ec70616eb618e7a4d083e54ffcf818d071dd6
Vicky <vicky.nseng@gmail.com>
2018-08-12 07:28:45 +0200

    Define interface for each slice of state Compose states for the global application state Strong type the state Add initial value of the state Build selectors

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -1,4 +1,45 @@
-export function reducer(state, action) {
+import { Product } from '../product';
+import * as fromRoot from '../../state/app.state';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+
+// For lazy loading
+export interface State extends fromRoot.State {
+  products: ProductState;
+}
+
+// Product state
+export interface ProductState {
+  showProductCode: boolean;
+  currentProduct: Product;
+  products: Product[];
+}
+
+// Initial state
+const initialState: ProductState = {
+  showProductCode: true,
+  currentProduct: null,
+  products: []
+};
+
+// Selectors
+const getProductFeatureState = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(
+  getProductFeatureState,
+  state => state.showProductCode
+);
+
+export const getCurrentProduct = createSelector(
+  getProductFeatureState,
+  state => state.currentProduct
+);
+
+export const getProducts = createSelector(
+  getProductFeatureState,
+  state => state.products
+);
+
+export function reducer(state = initialState, action): ProductState {
   switch (action.type) {
     case 'TOOGLE_PRODUCT_CODE':
       return {
